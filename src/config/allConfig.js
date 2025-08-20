69dd49342e00cb13a244f6434a761bd9c4d18475
Priyanshu Singh <priyanshurajrajput93@gmail.com>
2025-08-20 10:41:43 +0530

    Bug fixed

--- src/config/allConfig.js
+++ src/config/allConfig.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const DB_URL = process.env.DB_URL;
+export const DB_URL = process.env.DB_URl;
 export const SECRET_KEY = process.env.SECRET_KEY;
 export const PORT = process.env.PORT || 3000;
